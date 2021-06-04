b8a4f18802932b4e1c32f599184921c2f7f04115
shivammathur4 <shivamm.4aug@gmail.com>
2021-06-04 10:32:42 +0530

    added method for postapi

--- AddressBookTest/UnitTest1.cs
+++ AddressBookTest/UnitTest1.cs
@@ -17,9 +17,10 @@ namespace AddressBookTest
         [TestInitialize]
         public void SetUp()
         {
-            client = new RestClient("http://localhost:5000/contacts");
+            client = new RestClient("http://localhost:5000");
         }
 
+       
         private IRestResponse GetContactList()
         {
             RestRequest request = new RestRequest("/contacts/list", Method.GET);
@@ -29,15 +30,36 @@ namespace AddressBookTest
 
         
         [TestMethod]
-        public void ReadEntriesFromJsonServer()
+        public void OnCallingPostAPIForAContactListWithMultipleContacts_ReturnContactObject()
         {
-            IRestResponse response = GetContactList();
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-            List<Contact> employeeList = JsonConvert.DeserializeObject<List<Contact>>(response.Content);
-            Assert.AreEqual(4, employeeList.Count);
-            foreach (Contact c in employeeList)
+            List<Contact> contactList = new List<Contact>();
+            contactList.Add(new Contact { FirstName = "Ramya", LastName = "U", PhoneNo = "9577456345", Address = "Feroz Shah Kotla", City = "New Delhi", State = "New Delhi", Zip = "547677", Email = "vs@gmail.com" });
+            contactList.Add(new Contact { FirstName = "Ankitha", LastName = "H", PhoneNo = "9756723456", Address = "Chinnaswamy", City = "Bangalore", State = "Karnataka", Zip = "435627", Email = "yc@gmail.com" });
+            contactList.Add(new Contact { FirstName = "Yashu", LastName = "V", PhoneNo = "9954564345", Address = "Mohali", City = "Mohali", State = "Punjab", Zip = "113425", Email = "klr@gmail.com" });
+
+            foreach (var v in contactList)
             {
-                Console.WriteLine($"Id: {c.Id}\tFullName: {c.FirstName} {c.LastName}\tPhoneNo: {c.PhoneNumber}\tAddress: {c.Address}\tCity: {c.City}\tState: {c.State}\tZip: {c.Zip}\tEmail: {c.Email}");
+                RestRequest request = new RestRequest("/contacts/list", Method.POST);
+                JsonObject jsonObj = new JsonObject();
+                jsonObj.Add("firstname", v.FirstName);
+                jsonObj.Add("lastname", v.LastName);
+                jsonObj.Add("phoneNo", v.PhoneNo);
+                jsonObj.Add("address", v.Address);
+                jsonObj.Add("city", v.City);
+                jsonObj.Add("state", v.State);
+                jsonObj.Add("zip", v.Zip);
+                jsonObj.Add("email", v.Email);
+
+                request.AddParameter("application/json", jsonObj, ParameterType.RequestBody);
+
+                IRestResponse response = client.Execute(request);
+
+                Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+                Contact contact = JsonConvert.DeserializeObject<Contact>(response.Content);
+                Assert.AreEqual(v.FirstName, contact.FirstName);
+                Assert.AreEqual(v.LastName, contact.LastName);
+                Assert.AreEqual(v.PhoneNo, contact.PhoneNo);
+                Console.WriteLine(response.Content);
             }
         }
     }
